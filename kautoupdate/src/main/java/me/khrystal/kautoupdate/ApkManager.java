cf49ce965b7fe64c13ad59851fbeabd39955c65b
kHRYSTAL <723526676@qq.com>
2016-10-13 15:36:56 +0800

    add bsdiff

---
+++ kautoupdate/src/main/java/me/khrystal/kautoupdate/ApkManager.java
@@ -0,0 +1,22 @@
+package me.khrystal.kautoupdate;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+
+/**
+ * usage:
+ * author: kHRYSTAL
+ * create time: 16/10/13
+ * update time:
+ * email: 723526676@qq.com
+ */
+
+public class ApkManager {
+
+    public static String getApkPath(Context context) {
+        context = context.getApplicationContext();
+        ApplicationInfo info = context.getApplicationInfo();
+        String apkPath = info.sourceDir;
+        return apkPath;
+    }
+}
